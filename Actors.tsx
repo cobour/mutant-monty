9177d6f00a1527fa1807b5011287da6d42675bd0
Frank <cobour@web.de>
2025-09-08 18:22:18 +0200

    first enemy

--- Actors.tsx
+++ Actors.tsx
@@ -6,4 +6,9 @@
       <property name="Name" value="player_actor" type="string" />
     </properties>
   </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="enemy_green_two_eye_actor" type="string" />
+    </properties>
+  </tile>
 </tileset>
